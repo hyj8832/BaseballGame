30ba3e58be989e1b3715479ec8af8b807123901a
Yiju Hong <hyj8832@naver.com>
2018-04-21 20:30:10 +0900

    first commit

---
+++ BaseballGame/BaseballGameEngine.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace BaseballGame
+{
+    internal class BaseballGameEngine
+        {
+        int[] quiz = new int[3];
+        int stlike = 0;
+        int ball = 0;
+      
+      
+        
+        internal void Init()
+        {
+            //quiz내기 , 1~9까지 중복없이 배열 만들자 랜덤하게 두 자리뽑고, 교환(교체) 하자 앞 세자리를 quiz배열에 넣기
+            int[] temp = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            Random rand = new Random();
+
+            for (int n = 0; n < 100; n++) //중복 없도록 100번섞기
+            {
+                int i = rand.Next(0, temp.Length);//(0,9) 0~9에서 정수만 뽑아
+                int j = rand.Next(0, temp.Length);
+
+                int t = temp[i];
+                temp[i] = temp[j];
+                temp[j] = t;
+            }
+
+               for(int i = 0; i < quiz.Length; i++) {
+                quiz[i]=temp[i];
+                    }
+          
+
+            return;
+        }
+
+        internal void Play(string input)
+        {
+            //quiz랑 input이랑 비교해서 stlike,ball판정하자
+            //quiz를 for문으로 돌고
+            //input을 for문으로 돌고
+            //하나씩 꺼내어 숫자가 같으면
+            //자리수 같으면 stlike++
+            //자릿수가 다르면 ball++
+            stlike = 0;
+            ball = 0;
+
+            //Console.WriteLine("quiz:{0}{1}{2}{3}", quiz[0], quiz[1], quiz[2], quiz[3]);
+            int[] inputArray = new int[input.Length];
+            for (int i = 0; i < input.Length; i++)
+            {
+                inputArray[i] = input[i] - '0'; //'1'-'0'=1이다. 49-48이라서
+            }
+            for(int i = 0; i < quiz.Length; i++)
+            {
+                for(int j = 0; j < inputArray.Length; j++)
+                {
+                    if (quiz[i] == inputArray[j]) //꺼낸 숫자가 같다면~
+                    {
+                        if (i == j)//자릿수 까지 같다면
+                        {
+                            stlike++;
+                        }
+                        else
+                        {
+                            ball++;
+                        }
+                    }
+                }
+            }
+            
+            return;
+        }
+
+        internal int GetQuizLength()
+        {
+            return quiz.Length;
+        }
+
+        internal int GetBall()
+        {
+            return ball;
+        }
+
+       internal int GerStrike()
+        {
+            return stlike;
+        }
+       
+    }
+}
---
+++ BaseballGame/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseballGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //BaseballGameEngine 생성하자
+            //게임엔진 초기화하자: quiz만들자(랜덤, 중복없어야한다)
+            //quiz는 중복없이 랜덤한 숫자로 만들자
+            //무한반복해서 사용자 입력을 받는다
+            //게임엔진에 사용자 입력을 넘기고, strike, ball 판정한다.
+            //strike의 개수와 quiz길이가 같으면 끝. 축하해주자.
+            BaseballGameEngine bge = new BaseballGameEngine();
+            bge.Init(); //quiz낸다
+            while (true)
+            {
+                Console.Write("Input Number: ");
+                string input = Console.ReadLine();
+                bge.Play(input); //Play라는 함수로 가서 사용자가 입력한 값 처리
+                Console.WriteLine("input : {0}\tStrike: {1}\tball: {2}",input,bge.GerStrike(),bge.GetBall());
+                if (bge.GerStrike()==bge.GetQuizLength())// if (bge.GerStrike()==input.Length) 이렇게 하면 엔터키치면 잘 맞췄다고 나옴. 
+                {
+                    Console.WriteLine("축하합니다. 잘 맞췄어요.");
+                    break;
+                }
+            }
+        }
+    }
+}
